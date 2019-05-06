44320c08f1a37bc74cf310ed1c59125639f7778d
Milind Joshi <Milind Joshi>
2019-05-06 14:11:33 +0530

    add

---
+++ src/main/java/com/example/demo/EntryServiceApplication.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/entry")
+public class EntryServiceApplication {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EntryServiceApplication.class, args);
+	}
+	
+	@RequestMapping("/localwelcome")
+	public String localwelcome()
+	{
+		return "Returned from EntryService Itself";
+	}
+	
+	@RequestMapping("/welcome")
+	public String welcome()
+	{
+		String calledFrom="EntryService";
+		String response = restTemplate
+                .exchange("http://localhost:8081/app/welcome/{calledFrom}"
+                , HttpMethod.GET
+                , null
+                , new ParameterizedTypeReference<String>() {
+            }, calledFrom).getBody();
+		return response;
+	}
+	
+	
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    } 
+
+}
